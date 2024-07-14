c29739ccabbd79299a0c2b75108f1a275d48173f
KUNDANYADAV7 <141399895+KUNDANYADAV7@users.noreply.github.com>
2024-07-14 21:13:56 +0530

    Create SplashScreen.js

---
+++ screens/SplashScreen.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const SplashScreen = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.navigate('Login');
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [navigation]);
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{ uri: 'https://aqad.ae/wp-content/uploads/2023/07/aqad-logo-1.png' }}
+        style={styles.logo}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+  },
+});
+
+export default SplashScreen;
