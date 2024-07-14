d8ae2f3bdc4bf3f753cda4738cc15f803ba6bbca
KUNDANYADAV7 <141399895+KUNDANYADAV7@users.noreply.github.com>
2024-07-14 21:14:30 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,38 +1,25 @@
-// In App.js in a new project
-
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/LoginScreen';
-import SignupScreen from './screens/SignupScreen';
-import { auth } from './firebase/firebase.config';
 import { ToastProvider } from 'react-native-toast-notifications';
-import ContactScreen from './screens/ContactScreen';
-import HomeScreen from './screens/HomeScreen';
-import DashboardScreen from './screens/DashboardScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import SignupListScreen from './screens/SignupListScreen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import 'react-native-gesture-handler';
+import {AuthProvider } from './Context/AuthContext';
+import MainApp from './screens/MainApp';
 
-
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 function App() {
   return (
+    
+    <AuthProvider>
     <NavigationContainer>
-       <ToastProvider>
-      <Stack.Navigator initialRouteName='Login' screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Signup" component={SignupScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Contact" component={ContactScreen} />
-        <Stack.Screen name="Dashboard" component={DashboardScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-        <Stack.Screen name="Signuplist" component={SignupListScreen} />
-      </Stack.Navigator>
+      <ToastProvider>
+        <MainApp />
       </ToastProvider>
     </NavigationContainer>
+    </AuthProvider>
   );
 }
+
 
 export default App;
