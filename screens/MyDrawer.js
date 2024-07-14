d5f7f565e8779e722f8f16c103383c090d75a9df
KUNDANYADAV7 <141399895+KUNDANYADAV7@users.noreply.github.com>
2024-07-14 14:49:04 +0530

    Update MyDrawer.js

--- screens/MyDrawer.js
+++ screens/MyDrawer.js
@@ -1,31 +1,3 @@
-// import * as React from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createDrawerNavigator } from '@react-navigation/drawer';
-// import HomeScreen from './screens/HomeScreen';
-// import ProfileScreen from './screens/ProfileScreen';
-// import ContactScreen from './screens/ContactScreen';
-// import SignupListScreen from './screens/SignupListScreen';
-// import DashboardScreen from './screens/DashboardScreen';
-// import { View, Text, TouchableOpacity } from 'react-native';
-// import { useNavigation } from '@react-navigation/native';
-// import { signOut } from '../firebase/authService';
-
-// const Drawer = createDrawerNavigator();
-
-// export default function MyDrawer() {
-//   return (
-//     <Drawer.Navigator initialRouteName="Home">
-//       <Drawer.Screen name="Home" component={HomeScreen} />
-//       <Drawer.Screen name="Profile" component={ProfileScreen} />
-//       <Drawer.Screen name="Contact" component={ContactScreen} />
-//       <Drawer.Screen name="Signup List" component={SignupListScreen} />
-//       <Drawer.Screen name="Dashboard" component={DashboardScreen} />
-//     </Drawer.Navigator>
-//   );
-// }
-
-
-// src/MyDrawer.js
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from './HomeScreen';
