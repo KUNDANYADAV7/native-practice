63994e0df7a9dfce209db2992f9931c6866ed13b
KUNDANYADAV7 <141399895+KUNDANYADAV7@users.noreply.github.com>
2024-07-14 21:10:08 +0530

    Update LoginScreen.js

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,10 @@
-
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Image, StatusBar } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image, StatusBar, StyleSheet } from 'react-native';
 import Animated, { FadeInUp, FadeInDown } from 'react-native-reanimated';
 import { useNavigation } from '@react-navigation/native';
 import { useToast } from 'react-native-toast-notifications';
-import { login } from '../firebase/authService';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useAuth } from '../Context/AuthContext';
 
 export default function LoginScreen() {
   const [email, setEmail] = useState('');
@@ -13,6 +12,8 @@ export default function LoginScreen() {
   const [errors, setErrors] = useState({});
   const navigation = useNavigation();
   const toast = useToast();
+
+  const { login } = useAuth();
 
   const validate = () => {
     let valid = true;
@@ -52,62 +53,62 @@ export default function LoginScreen() {
       toast.show('You have successfully logged in!', { type: 'success' });
       navigation.navigate('Home'); // Assuming 'Home' is the name of your home screen
 
-       // Retrieve and log stored user data from AsyncStorage
-       const storedUserData = await AsyncStorage.getItem('userData');
-       console.log("Stored user data:", storedUserData);
+      // Retrieve and log stored user data from AsyncStorage
+      const storedUserData = await AsyncStorage.getItem('userData');
+      console.log("Stored user data:", storedUserData);
     } catch (error) {
       toast.show(error.message, { type: 'danger' });
     }
   };
 
   return (
-    <View className="bg-white h-full w-full">
-      <StatusBar barStyle="light"/>
-      <Image className="h-full w-full absolute" source={require('../assets/images/background.png')}/>
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content"/>
+      <Image style={styles.backgroundImage} source={require('../assets/images/background.png')}/>
 
-      <View className="flex-row justify-around w-full absolute">
-        <Animated.Image entering={FadeInUp.delay(200).duration(1000).springify().damping(3)} className="h-[225] w-[90]" source={require('../assets/images/light.png')}/>
-        <Animated.Image entering={FadeInUp.delay(400).duration(1000).springify().damping(3)} className="h-[160] w-[65]" source={require('../assets/images/light.png')}/>
+      <View style={styles.lightImagesContainer}>
+        <Animated.Image entering={FadeInUp.delay(200).duration(1000).springify().damping(3)} style={styles.lightImageLarge} source={require('../assets/images/light.png')}/>
+        <Animated.Image entering={FadeInUp.delay(400).duration(1000).springify().damping(3)} style={styles.lightImageSmall} source={require('../assets/images/light.png')}/>
       </View>
 
-      <View className="h-full w-full flex justify-around pt-40">
-        <View className="flex items-center">
-          <Animated.Text entering={FadeInUp.duration(1000).springify()} className="text-white font-bold tracking-wider text-5xl">
+      <View style={styles.contentContainer}>
+        <View style={styles.headerContainer}>
+          <Animated.Text entering={FadeInUp.duration(1000).springify()} style={styles.headerText}>
             Login
           </Animated.Text>
         </View>
 
-        <View className="flex items-center mx-4 space-y-1">
-          <Animated.View entering={FadeInDown.duration(1000).springify()} className="bg-black/5 p-5 rounded-2xl w-full mb-3">
+        <View style={styles.formContainer}>
+          <Animated.View entering={FadeInDown.duration(1000).springify()} style={styles.inputContainer}>
             <TextInput
               placeholder='Email'
               placeholderTextColor={'gray'}
               value={email}
               onChangeText={setEmail}
-              style={{ height: 40, paddingVertical: 0 }}
+              style={styles.input}
             />
-            {errors.email && <Text className="text-red-500">{errors.email}</Text>}
+            {errors.email && <Text style={styles.errorText}>{errors.email}</Text>}
           </Animated.View>
-          <Animated.View entering={FadeInDown.delay(200).duration(1000).springify()} className="bg-black/5 p-5 rounded-2xl w-full mb-3">
+          <Animated.View entering={FadeInDown.delay(200).duration(1000).springify()} style={styles.inputContainer}>
             <TextInput
               placeholder='Password'
               placeholderTextColor={'gray'}
               secureTextEntry
               value={password}
               onChangeText={setPassword}
-              style={{ height: 40, paddingVertical: 0 }}
+              style={styles.input}
             />
-            {errors.password && <Text className="text-red-500">{errors.password}</Text>}
+            {errors.password && <Text style={styles.errorText}>{errors.password}</Text>}
           </Animated.View>
-          <Animated.View entering={FadeInDown.delay(400).duration(1000).springify()} className="w-full">
-            <TouchableOpacity className="w-full bg-sky-400 p-3 rounded-2xl mb-3" onPress={handleLogin}>
-              <Text className="text-xl font-bold text-white text-center">Login</Text>
+          <Animated.View entering={FadeInDown.delay(400).duration(1000).springify()} style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={handleLogin}>
+              <Text style={styles.buttonText}>Login</Text>
             </TouchableOpacity>
           </Animated.View>
-          <Animated.View entering={FadeInDown.delay(600).duration(1000).springify()} className="flex-row justify-center">
+          <Animated.View entering={FadeInDown.delay(600).duration(1000).springify()} style={styles.signupContainer}>
             <Text>Don't have an account? </Text>
-            <TouchableOpacity onPress={() => navigation.push('Signup')}>
-              <Text className="text-sky-600">SignUp</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('Signup')}>
+              <Text style={styles.signupText}>SignUp</Text>
             </TouchableOpacity>
           </Animated.View>
         </View>
@@ -115,3 +116,85 @@ export default function LoginScreen() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  backgroundImage: {
+    height: '100%',
+    width: '100%',
+    position: 'absolute',
+  },
+  lightImagesContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    position: 'absolute',
+  },
+  lightImageLarge: {
+    height: 225,
+    width: 90,
+  },
+  lightImageSmall: {
+    height: 160,
+    width: 65,
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: 'space-around',
+    paddingTop: 40,
+  },
+  headerContainer: {
+    alignItems: 'center',
+  },
+  headerText: {
+    color: 'white',
+    fontWeight: 'bold',
+    letterSpacing: 1,
+    fontSize: 40,
+    marginTop:100
+  },
+  formContainer: {
+    alignItems: 'center',
+    marginHorizontal: 16,
+  },
+  inputContainer: {
+    backgroundColor: 'rgba(0, 0, 0, 0.05)',
+    padding: 16,
+    borderRadius: 16,
+    width: '100%',
+    marginBottom: 12,
+  },
+  input: {
+    height: 40,
+    paddingVertical: 0,
+  },
+  errorText: {
+    color: 'red',
+  },
+  buttonContainer: {
+    width: '100%',
+  },
+  button: {
+    backgroundColor: '#38bdf8',
+    padding: 16,
+    borderRadius: 16,
+    marginBottom: 12,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
+  },
+  signupContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  signupText: {
+    color: '#0284c7',
+  },
+});
+
